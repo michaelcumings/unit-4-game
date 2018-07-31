33c56d0a94f538ea0c678e3a301bb4c39a75696b
Michael Cumings <michael.cumings@gmail.com>
2018-07-31 03:32:40 -0500

    bootstrap and some pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+// crystal values random between 1 and 12
+// random number betwen 19 and 120
+// variables
+// brown value
+// diamond value
+// purple value
+// red value
+// target number
+// total score
+// wins
+// losses
+
+var brownvalue = 0; // id brown
+var diamondvalue = 0; // id diamond
+var purplevalue = 0;  // id purple
+var redvalue = 0;  // id red
+var targetnum = 0;  // id target
+var totalnum = 0; // id total
+var numwins = 0; // id wins
+var numlosses = 0; // id losses
+
+// function for picking values for all the gems
+// random 1-12 for brown
+// do random 1-12 for diamond while diamond === brown
+// do random 1-12 for purple while purple === brown or purple === diamond
+// do random 1-12 for red while red === brown or red === diamond or red === purple
+
+
+// function for new game
+// do random 19-120 for target
+// overwrite target div with new target value
+// overwrite total div with 0
+// overwrite wins with numwins
+// overwrite losses with numlosses
+
+// win/loss check function 
+// if targetnum === totalnum, wins++, newtargetandtotal, picking
+// else if targetnum < totalnum, losses++, newtargetandtotal, picking
+
+ 
